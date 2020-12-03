9d97b790176feaf02d14339b38ac6e5452c803c4
Uncastellum <36407150+Uncastellum@users.noreply.github.com>
2020-12-03 22:03:21 +0100

    adding max to query

--- src/main/java/soa/eip/Router.java
+++ src/main/java/soa/eip/Router.java
@@ -1,5 +1,7 @@
 package soa.eip;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
@@ -8,11 +10,33 @@ public class Router extends RouteBuilder {
 
   public static final String DIRECT_URI = "direct:twitter";
 
+  private class MaxProc implements Processor{
+
+    public void process(Exchange exchange) throws Exception {
+      String exch = exchange.getIn().getBody(String.class);
+      String body = "", max = null;
+
+      for(String it: exch.split(" ")){
+        if(it.matches("max:[0-9]+")){
+          max = it.substring(4);
+        } else {
+          body = body + " " + it;
+        }
+      }
+
+      if(max != null) body += "?count=" + max;
+      
+      exchange.getOut().setBody(body);
+    }
+
+  }
+
   @Override
   public void configure() {
     from(DIRECT_URI)
       .log("Body contains \"${body}\"")
       .log("Searching twitter for \"${body}\"!")
+      .process(new MaxProc())
       .toD("twitter-search:${body}")
       .log("Body now contains the response from twitter:\n${body}");
   }
